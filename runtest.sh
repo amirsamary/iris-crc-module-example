100d7746d14f348e136d022cbef7ca7800925f86
Amir Samary <amirsamary@gmail.com>
2019-11-14 15:46:47 -0500

    Running the routine automatically on the right namespace.

--- runtest.sh
+++ runtest.sh
@@ -2,5 +2,5 @@
 
 docker run --rm -d --name test amirsamary/iris-crc-module-example:test 
 sleep 3
-docker exec -it test iris session iris
+docker exec -it test iris session iris -UAPP ^TEST
 docker stop test
